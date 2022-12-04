53fdf39547876e39e59931663934ad03c4e83781
Na Lin <lna483018@gmail.com>
2022-12-04 10:17:39 -0500

    implement programmatically navigation with V5 useHistory hook

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,17 @@
 import QuoteForm from "../components/quotes/QuoteForm";
+import { useHistory } from "react-router-dom";
 
 const NewQuote = () => {
+  // give back a history object
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    //push -> push a new page on the stack of pages
+    //replace -> replace the current page
+    // push can go back with back button, replace can't
+    history.push("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
