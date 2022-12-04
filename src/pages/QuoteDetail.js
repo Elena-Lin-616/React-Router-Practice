301f83f667b43074e0e6560251cfa01e18dcc641
Na Lin <lna483018@gmail.com>
2022-12-04 10:07:15 -0500

    add dynamic link to -> /quotes/:quoteId

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,17 +1,31 @@
+import { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
+
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
-function QuoteDetail() {
-  const { quoteId } = useParams();
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Maximilian", text: "Learning React is great!" },
+];
+
+const QuoteDetail = () => {
+  const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
 
   return (
-    <div>
-      <h1>quote detail</h1>
-      <p>{quoteId}</p>
-      <Route path="/quotes/:quoteId/comments">
+    <Fragment>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
-    </div>
+    </Fragment>
   );
-}
+};
 
 export default QuoteDetail;
