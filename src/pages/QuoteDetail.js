273366a34da0b090466ace62ee4912284b421ef1
Na Lin <lna483018@gmail.com>
2022-12-04 11:17:24 -0500

    Nested Route, use router to conditional render component without manage state

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
@@ -18,9 +18,19 @@ const QuoteDetail = () => {
     return <p>No quote found!</p>;
   }
 
+  // use React Router to conditional render component without managing complex state by Route
+
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
+
+      <Route path="/quotes/:quoteId" exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            See comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
